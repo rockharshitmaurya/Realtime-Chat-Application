ddf43fc2edd99dfddf55fb84868f64da91b77cbc
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-12-07 17:53:45 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://127.0.0.1:5000";
+export const host = "https://realtime-chat-application-phi.vercel.app";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
